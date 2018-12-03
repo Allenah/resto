fb68d6e7e69c56698ef1db0f06d7543aa715687c
Paul Busby <paul.busby@gmail.com>
2018-12-03 16:39:29 +0000

    Added empty controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+devise_for :users
 root to: 'restaurants#index'
   resources :restaurants, only: [:index, :show] do
     resources :reviews, only: [:new, :create]
