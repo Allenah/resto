d3ca97627ad94157ca7753bfd5fa91b200ec6423
Paul Busby <paul.busby@gmail.com>
2018-12-04 16:53:29 +0000

    Finished basic layout of show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,8 +2,17 @@ class RestaurantsController < ApplicationController
  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :capacity, :photo, :cuisine, :location, :price)
+  end
+
 end
