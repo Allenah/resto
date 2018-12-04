867d88741da0c52977ae3be7b085f02e642aff9b
Paul Busby <paul.busby@gmail.com>
2018-12-04 12:27:30 +0000

    Finished creating 10 seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Creating 10 restaurants...'
 
+puts "Cleaning up database..."
+Restaurant.destroy_all
 
 Restaurant.create!(
 name: 'Ramiro',
@@ -15,16 +17,18 @@ location: 'Lisbon',
 capacity: 60,
 price: 1000,
 cuisine: 'Portuguese',
+user_id: 1,
 photo: 'http://cityguidelisbon.files.wordpress.com/2014/03/ramiro.jpg'
 )
 
 Restaurant.create!(
 name: 'Quimera',
-description: 'Relaxed, casual 3-floor seafood and beer drinking eatery established in the 1950s.',
+description: 'Craft beers and comfort food set inside an ancient Roman tunnel',
 location: 'Lisbon',
 capacity: 30,
 price: 600,
 cuisine: 'Portuguese',
+user_id: 1,
 photo: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/ac133d63578377.5ab4ff8958b55.jpg'
 )
 
@@ -35,6 +39,7 @@ location: 'Lisbon',
 capacity: 100,
 price: 1200,
 cuisine: 'Portuguese',
+user_id: 1,
 photo: 'https://nit.pt/wp-content/uploads/2017/03/rio-maravilha-4.jpg'
 )
 
@@ -45,68 +50,74 @@ location: 'Lisbon',
 capacity: 120,
 price: 5000,
 cuisine: 'Portuguese',
+user_id: 1,
 photo: 'http://4.bp.blogspot.com/-DBH7A5-Pfpw/VlkAIi_noTI/AAAAAAAACbc/dflk_kk_t5g/s1600/20151116_Gin_Lovers_JS-2539.jpg'
 )
 
 Restaurant.create!(
 name: 'Bonsai',
-description: 'Relaxed, casual 3-floor seafood and beer drinking eatery established in the 1950s.',
+description: 'Sushi, sashimi & other Japanese dishes in a contemporary setting with traditional low tables.',
 location: 'Lisbon',
-capacity: 30,
-price: 600,
+capacity: 50,
+price: 4000,
 cuisine: 'Japanese',
-photo: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/ac133d63578377.5ab4ff8958b55.jpg'
+user_id: 1,
+photo: 'https://media.timeout.com/images/103697986/image.jpg'
 )
 
 Restaurant.create!(
-name: 'Quimera',
-description: 'Relaxed, casual 3-floor seafood and beer drinking eatery established in the 1950s.',
+name: 'Clandestino',
+description: 'Some of the best chinese food in hipster, graffiti settings',
 location: 'Lisbon',
-capacity: 30,
-price: 600,
-cuisine: 'Portuguese',
-photo: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/ac133d63578377.5ab4ff8958b55.jpg'
+capacity: 60,
+price: 1000,
+cuisine: 'Chinese',
+user_id: 1,
+photo: 'https://cdn.vox-cdn.com/thumbor/Erxs97xM_IaQLkStgNnY5jBP26Y=/0x0:5307x3538/920x0/filters:focal(0x0:5307x3538):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8756501/20170622JF_EATER_CHINESE_RESTAURANTS_18.jpg'
 )
 
 Restaurant.create!(
-name: 'Quimera',
-description: 'Relaxed, casual 3-floor seafood and beer drinking eatery established in the 1950s.',
+name: 'Chutnify',
+description: 'Stylish decor and delicious curries in one of the posher parts of town',
 location: 'Lisbon',
-capacity: 30,
-price: 600,
-cuisine: 'Portuguese',
-photo: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/ac133d63578377.5ab4ff8958b55.jpg'
+capacity: 50,
+price: 4000,
+cuisine: 'Indian',
+user_id: 1,
+photo: 'https://magnolia-portugal.dunegestion.com/w4/php/dune/file.php?file=94706_chutnify2.jpg&hash=af22e99a9d'
 )
 
 Restaurant.create!(
-name: 'Quimera',
-description: 'Relaxed, casual 3-floor seafood and beer drinking eatery established in the 1950s.',
+name: 'Moules & Beer',
+description: 'Quality beers and seafood in a bright and open environment',
 location: 'Lisbon',
-capacity: 30,
-price: 600,
-cuisine: 'Portuguese',
-photo: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/ac133d63578377.5ab4ff8958b55.jpg'
+capacity: 80,
+price: 3000,
+cuisine: 'Belgian',
+user_id: 1,
+photo: 'https://apis.infoportugal.info/cms-media/pois/final/141/JRN.RE.28423-141901.jpg'
 )
 
 Restaurant.create!(
-name: 'Quimera',
-description: 'Relaxed, casual 3-floor seafood and beer drinking eatery established in the 1950s.',
+name: "La Brasserie de LEntrecôte",
+description: 'Steak & frites is the focus of this classically simple bistro, also serving vegetarian options.',
 location: 'Lisbon',
-capacity: 30,
-price: 600,
-cuisine: 'Portuguese',
-photo: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/ac133d63578377.5ab4ff8958b55.jpg'
+capacity: 80,
+price: 6000,
+cuisine: 'French',
+user_id: 1,
+photo: 'https://www.visitlisboa.com/sites/default/files/2016-09/1136-la-brasserie-de-l-entrecote%20%281%29.JPG'
 )
 
 Restaurant.create!(
-name: 'Quimera',
-description: 'Relaxed, casual 3-floor seafood and beer drinking eatery established in the 1950s.',
+name: 'Bairro do Avillez',
+description: 'Another hit from José Avillez, who recently opened this restaurant complex, designed to look like a typical Portuguese neighborhood.',
 location: 'Lisbon',
-capacity: 30,
-price: 600,
+capacity: 300,
+price: 10000,
 cuisine: 'Portuguese',
-photo: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/ac133d63578377.5ab4ff8958b55.jpg'
+user_id: 1,
+photo: 'https://cdn.vox-cdn.com/thumbor/ruJOg1jkiwnpOH3WJkno1h5Xdc0=/0x0:1280x853/970x728/filters:focal(538x325:742x529):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/62561177/8_Lisbon_BairrodoAvillez.0.0.jpg'
 )
-
 
 puts "Created 10 restaurant seeds!"
