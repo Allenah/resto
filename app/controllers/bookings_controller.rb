5bf224528ca9d565f389d5117cef1de59396cd60
Allenah Herholdt allenah@silvercoasthospitality.com <allenah@silvercoasthospitality.com>
2018-12-04 17:00:46 +0000

    started booking with flatpickr and changed restaurent-card.scss file

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,26 @@
 class BookingsController < ApplicationController
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @booking = Booking.new
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @booking = Booking.new(booking_params)
+    @booking.restaurant = @restaurant
+    if @booking.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time)
   end
 end
