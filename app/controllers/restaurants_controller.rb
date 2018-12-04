f49d45f1197661bdecd770aec7927f9b9f056604
Paul Busby <paul.busby@gmail.com>
2018-12-04 17:23:34 +0000

    Fixed the showpage branch

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,10 +9,4 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
-  private
-
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :description, :capacity, :photo, :cuisine, :location, :price)
-  end
-
 end
