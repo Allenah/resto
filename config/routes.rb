55ef8203ad355f0a842603da4c9a0fa1beef05b1
Carolina Lemos <carolinaperestrelodelemos@gmail.com>
2018-12-06 12:51:12 +0000

    Landing page finished with about info

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 devise_for :users
-root to: 'restaurants#index'
+root to: 'pages#home'
   resources :restaurants, only: [:index, :show] do
     resources :reviews, only: [:new, :create, :show]
     resources :bookings, only: [:new, :create, :show]
