dc3872594aa38b5dc2a605b9949933ec4b2512d9
Paul Busby <paul.busby@gmail.com>
2018-12-06 16:19:18 +0000

    Added coordinates to restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_110038) do
+ActiveRecord::Schema.define(version: 2018_12_06_161755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2018_12_05_110038) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "photo"
+    t.float "lat"
+    t.float "long"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
